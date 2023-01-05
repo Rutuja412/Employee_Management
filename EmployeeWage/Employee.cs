0f59862d25ce0543c5284b66bc37d4569b01f284
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-05 10:42:25 +0530

    [Rutuja]:Adding Part Time Employee

--- EmployeeWage/Employee.cs
+++ EmployeeWage/Employee.cs
@@ -8,12 +8,18 @@ namespace EmployeeWage
     {
         public static void EmployeeAttendance()
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Partially Present");
+                empHrs = 4;
+            }
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
